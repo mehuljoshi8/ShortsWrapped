3a5415a9d9c81405674fbd79057449f7e1dc492d
Mehul Joshi <mehuljoshi@Mehuls-MacBook-Pro.local>
2023-08-22 01:03:33 -0700

    cleaned up main; extracted requestParms from gin.Context

--- server.go
+++ server.go
@@ -4,38 +4,52 @@ import (
     "fmt"
     "net/http"
     "io"
-    "os"
     "github.com/gin-gonic/gin"
     "github.com/twilio/twilio-go/twiml"
+    "net/url"
 )
 
-// TODO: Implement a function that takes the string
-// body and returns a map from string -> string.
-// stringToJsonMap()
+// TODO: Complete database work
+// users, links, etc, etc tables
+// pulling, querying, etc, etc, etc
+
+// then link that to this
+
+func smsHandler(context *gin.Context) {
+    body, err := io.ReadAll(context.Request.Body)
+    if err != nil {
+        context.Abort()
+        return 
+    }
+
+    // type(reqParams) = map[string]string
+    reqParams, err := url.ParseQuery(string(body))
+    if err != nil {
+        context.Abort()
+        return
+    }
+
+    for k := range reqParams {
+        fmt.Println(k)
+    }
+
+    fmt.Println(reqParams["Body"])
+
+    message := &twiml.MessagingMessage {
+        Body: "Let the chaos begin",
+    }
+
+    twimlResult, err := twiml.Messages([]twiml.Element{message})
+    if err != nil {
+        context.String(http.StatusInternalServerError, err.Error())
+    } else {
+        context.Header("Content-Type", "text/xml")
+        context.String(http.StatusOK, twimlResult)
+    }
+}
 
 func main() {
     router := gin.Default()
-
-    router.POST("/sms", func(context *gin.Context) {
-        body, err := io.ReadAll(context.Request.Body)
-        fmt.Println("hello world")
-        if err != nil {
-            os.Exit(0)    
-        }
-
-        fmt.Println(string(body))
-        message := &twiml.MessagingMessage {
-            Body: "Amoli is coming! RUNNNN",
-        }
-
-        twimlResult, err := twiml.Messages([]twiml.Element{message})
-        if err != nil {
-            context.String(http.StatusInternalServerError, err.Error())
-        } else {
-            context.Header("Content-Type", "text/xml")
-            context.String(http.StatusOK, twimlResult)
-        }
-    })
-
+    router.POST("/sms", smsHandler)
     router.Run(":4040")
 }
